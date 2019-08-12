4e20f150eeefb332ce9355ecb6b5a91c1693a69a
Steven Bull <stevenwbulljr@gmail.com>
2019-08-12 14:55:45 -0500

    added try/catch block

--- function-drills.js
+++ function-drills.js
@@ -1,12 +1,20 @@
 function getYearOfBirth(age) {
-    return 2019 - age;
+  if (age < 0) {
+    throw new Error('Age can not be negative!!!');
+  }
+  return 2019 - age;
 }
 
 function createGreeting(name, age) {
-    return `Hi, my name is ${name} and I'm ${age} years old and I was born in ` + getYearOfBirth(age) + '.';
+  return `Hi, my name is ${name} and I'm ${age} years old and I was born in ` + getYearOfBirth(age) + '.';
 }
 
-const greeting1 = createGreeting();
+try {
+  const greeting1 = createGreeting('Steven', -12);
+} catch (error) {
+  console.error(error);
+}
+
 console.log(greeting1);
 
 
