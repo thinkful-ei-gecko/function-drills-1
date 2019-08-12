cb4e2542aee8ff7483139580c7c9a0a03da58134
Steven Bull <stevenwbulljr@gmail.com>
2019-08-12 14:45:24 -0500

    added getYearOfBirth function

--- function-drills.js
+++ function-drills.js
@@ -1,6 +1,9 @@
+function getYearOfBirth(age) {
+    return 2019 - age;
+}
+
 function createGreeting(name, age) {
-    const yearOfBirth = 2019 - age;
-    return `Hi, my name is ${name} and I'm ${age} years old and I was born in ${yearOfBirth}`;
+    return `Hi, my name is ${name} and I'm ${age} years old and I was born in ` + getYearOfBirth(age) + '.';
 }
 
 const greeting1 = createGreeting();
