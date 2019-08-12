f615df772b305e0c4103e5234f561e935a2dc5b9
Steven Bull <stevenwbulljr@gmail.com>
2019-08-12 15:13:20 -0500

    added throw error for invalid name and age type

--- function-drills.js
+++ function-drills.js
@@ -8,16 +8,18 @@ function getYearOfBirth(age) {
 function createGreeting(name, age) {
   if (name === undefined || age === undefined) {
     throw new Error('Please enter in a valid name and age.');
+  } else if (typeof name !== 'string' || typeof age !== 'number') {
+    throw new Error('Please enter in a valid name and age...');
+  } else {
+    return `Hi, my name is ${name} and I'm ${age} years old and I was born in ` + getYearOfBirth(age) + '.';
   }
-  return `Hi, my name is ${name} and I'm ${age} years old and I was born in ` + getYearOfBirth(age) + '.';
 }
 
 try {
-  const greeting1 = createGreeting();
+  const greeting1 = createGreeting('Steven', 26);
 } catch (error) {
   console.error(error);
 }
 
+const greeting1 = createGreeting('Steven', 26);
 console.log(greeting1);
-
-
